61f792ff4a6fb35a96afca5b1e1d18a63e0a2eea
vaigtech <vaig.tech@gmail.com>
2018-02-09 17:16:07 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
         res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token waddadada');
+    res.send('Error, wrong token1');
 });
 
 /*
